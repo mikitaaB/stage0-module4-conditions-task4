bebd4536c4dfb399147c99e5c0605693d0d54f6e
Mikita Buinavets <mikita.buinavets@arua.co>
2024-02-25 14:33:11 +0300

    module 4. task 4

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        if (first >= third && first >= second) {
+            System.out.println(first);
+        } else if (second > first && second > third) {
+            System.out.println(second);
+        } else if (third > first && third > second) {
+            System.out.println(third);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int max = first > second ? first : second;
+        int res = max > third ? max : third;
+        System.out.println(res);
     }
 }
