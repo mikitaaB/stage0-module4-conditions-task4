b6f63264e5d35ea2ec07d9c102246cfee15e7003
Mikita Buinavets <mikita.buinavets@arua.co>
2024-03-04 03:13:53 +0300

    module 4. task 4. fix 3

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,7 +2,6 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
-        int num = first > second ? first : second;
-        System.out.println(num);
+        System.out.println(first > second ? "first" : "second");
     }
 }
